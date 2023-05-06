ad2ff4fd1448de682d4a2b6172f40bd64ad084d6
sunms0710 <sunms0710@naver.com>
2023-05-07 03:47:30 +0900

    ajax 통신 함수 개발

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,19 @@
+let index = {
+    init: function (){
+        $("#btn-save").on("click", ()=> {
+            this.save();
+        });
+    },
+
+    save: function (){
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+            email: $("#email").val()
+        }
+
+        $.ajax().done().fail();
+    }
+}
+
+index.init();
