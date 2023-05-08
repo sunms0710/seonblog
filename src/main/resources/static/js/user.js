8e46fd453ebf16d12e14eb35ad7d6ab198bfacdd
sunms0710 <sunms0710@naver.com>
2023-05-09 01:29:50 +0900

    회원가입 ajax 통신 구현

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -10,9 +10,21 @@ let index = {
             username: $("#username").val(),
             password: $("#password").val(),
             email: $("#email").val()
-        }
+        };
 
-        $.ajax().done().fail();
+        $.ajax({
+            type: "POST",
+            url: "/blog/api/user",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function(resp) {
+            alert("회원가입이 완료되었습니다.");
+            console.log(resp);
+            location.href="/blog";
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
     }
 }
 
