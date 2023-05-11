899fa7de12dfc3aa0337a664c418b20434fcb33a
sunms0710 <sunms0710@naver.com>
2023-05-12 01:54:15 +0900

    로그인 service 개발

--- src/main/java/com/seon/blog/repository/UserRepository.java
+++ src/main/java/com/seon/blog/repository/UserRepository.java
@@ -2,6 +2,13 @@ package com.seon.blog.repository;
 
 import com.seon.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    // JPA Naming 쿼리
+    // SELECT * FROM user WHERE username = ? AND password = ?
+    User findByUsernameAndPassword(String username, String password);
+
+//    @Query(value = "SELECT * FROM user WHERE username = ?1 AND password = ?2", nativeQuery = true)
+//    User login(String username, String password);
 }
--- src/main/java/com/seon/blog/service/UserService.java
+++ src/main/java/com/seon/blog/service/UserService.java
@@ -4,8 +4,8 @@ import com.seon.blog.model.User;
 import com.seon.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @Service
 public class UserService {
@@ -20,5 +20,10 @@ public class UserService {
     @Transactional
     public void memberJoin(User user) {
         userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true) //select 할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료(정합성)
+    public User memberLogin(User user) {
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
     }
 }
