d51c453c55861622da3cf17d0d069383fb9710a7
sunms0710 <sunms0710@naver.com>
2023-05-12 01:53:17 +0900

    로그인 api 개발

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
         $("#btn-save").on("click", ()=> {
             this.save();
         });
+        $("#btn-login").on("click", ()=> {
+            this.login();
+        });
     },
 
     save: function (){
@@ -14,14 +17,33 @@ let index = {
 
         $.ajax({
             type: "POST",
-            url: "/blog/api/user",
+            url: "/api/user",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
             dataType: "json"
         }).done(function(resp) {
             alert("회원가입이 완료되었습니다.");
-            console.log(resp);
-            location.href="/blog";
+            location.href="/";
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    login: function (){
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/api/user/login",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function(resp) {
+            alert("로그인이 완료되었습니다.");
+            location.href="/";
         }).fail(function(error) {
             alert(JSON.stringify(error));
         });
